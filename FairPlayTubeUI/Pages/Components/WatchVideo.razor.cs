c48832d325030d0d58a1ce0612ae7bbdf0e1db75
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-06-21 16:32:29 +0100

    🚧Video details component

---
+++ FairPlayTubeUI/Pages/Components/WatchVideo.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FairPlayTubeUI.Pages.Components
+{
+    public partial class WatchVideo : ComponentBase
+    {
+        [Parameter] public string? Name { get; set; }
+    }
+}
